09b45f36333d09469aef0e7dd0512f345606511d
Joel Ezell <joele@avaya.com>
2019-09-30 21:55:49 -0600

    Final comments

--- main.go
+++ main.go
@@ -27,11 +27,15 @@ func main() {
 	/*
 	* Basic design:
 	*
-	* The statistics package has a singleton that tracks the current instance and the average execution time. Both of these must be protected
-	* via a mutex.
-	* The passwords package features a singleton that maps the increment number (learned from the statistics package) to the hashed password
-	* Another singleton waitgroup exists. When a POST comes to the hash, the password package spins off a goroutine and adds the worker to
-	* the waitgroup. A call to a shutdown immediately stops the web server to prevent new requests from arriving. It then spins off a goroutine
+	* The statistics package has a singleton that tracks the total number of invocations and the average execution time.
+	* This data is protected by a mutex.
+	*
+	* The passwords package features a singleton that maps the index number to the corresponding hashed password. There is also a singleton
+	* counter to track the current index. Both of these are protected via mutex.
+	* Another singleton waitgroup exists. When a request to perform a new has arrives, the password package spins up a
+	* goroutine and adds the worker to the waitgroup.
+	*
+	* A call to a shutdown immediately stops the web server to prevent new requests from arriving. It then spins up a goroutine
 	* which waits for the group to complete. After it completes, the process exits
 	*
 	 */
